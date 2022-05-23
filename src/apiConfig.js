b807c8d6e6adec7821ffbe33fe88dff89cdbe544
jmballe <jm_balle@hotmail.com>
2022-05-22 23:29:55 -0300

    Added directory structure and api functions

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,61 @@
+import axios from axios
+
 export const API = {
-    baseUrl: 'https://api.themoviedb.org/3'
+    baseUrl: 'https://api.themoviedb.org/3',
+    apiKey: '?api_key=96e69c404cd0460e36c3a4dacacdd719',
+    entity:{
+        topRatedMovies: 'movie/top_rated',
+        topRatedSeries: '/tv/top_rated',
+        popularMovies: 'movie/popular',
+        popularSeries: '/tv/popular',
+    },
+    language: {
+        es: '&language=es-ES',
+        en: '&language=en-US',
+    },
+    pagination: '&page=',
+    imageUrl: 'https://image.tmdb.org/t/p',
+    imageQuality: {
+        posterSmall: '/w200',
+        posterMedium: '/w300',
+        posterLarge: '/w500',
+        backdropSmall: '/w300',
+        backdropMedium: '/w780',
+        backdropLarge: '/w1280',
+    }
 }
 
+export const createUrl = {
+    api: (entity, lang = 'es', page = 1) => {
+        const url = `${API.baseUrl}${entity}${API.apiKey}${API.language[lang]}${API.pagination}${page}`;
+        return url;
+    },
+    image: (path, quality) => {
+        const url = `${API.imageUrl}${API.imageQuality[quality]}${path}`;
+        return url;
+    }
+}
 
+export const apiBuilder = {
+    tryGet: async (entity,lang = 'es', page = 1) => {
+        const url = createUrl.api(entity,lang,page);
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            return error;
+        }
+    },
+    tryGetImage: async(path,quality = 'posterLarge') => {
+        const url = createUrl.image(path,quality);
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            return error;
+        }
+    }
 
-// https://api.themoviedb.org/3/movie/550?api_key=96e69c404cd0460e36c3a4dacacdd719
+}
---
+++ src/hooks/useApi.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+
+const useApi = (entity,lang = 'es',actualPage = 1) => {
+    const [values,setValues] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
+    const [page,setPage] = useState(actualPage);
+
+    const getData = async () => {
+        setLoading(true);
+        setError(null);
+
+        const res = await apiBuilder.tryGet(entity,lang, page);
+
+        if(res.lenth ===0){
+            setError('Error en la carga de datos');
+        } else {
+            setValues(res);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getData();
+    },[page]);
+
+    const [randomValue,setRandomValue] = useState(null);
+    const [randomImage,setRandomImage] = useState(null);
+
+    const getRandomValue = () => {
+        const randomIndex = Math.floor(Math.random() * values.length);
+        if(values.length === 0) {
+            return
+        } else {
+            const selectedValue = values[randomIndex(0,values.length - 1)];
+            setRandomValue(selectedValue);
+            const backGroundImage = apiBuilder.tryGetImg(
+                selectedValue.backdrop_path,
+                'backdropLarge'
+            );
+        }
+        setRandomImage(backGroundImage);
+    }
+
+    useEffect(() => {
+        getRandomValue();
+    },[values]);
+
+    return [values, loading, error, randomValue, randomImage];
+}
+
+export default useApi;
